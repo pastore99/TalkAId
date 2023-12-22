9a715af5bb5f228fd1ec8fb3023cf5f9f8ad6107
Sewaaa <sparno.samuele71@gmail.com>
2023-12-22 17:44:55 +0100

    session variable with information of logged user setted

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,12 +1,16 @@
 package controller;
 
+import model.DAO.DAOUser;
+import model.entity.User;
+import model.service.login.Authenticator;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import model.service.login.Authenticator;
 
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
@@ -33,7 +37,11 @@ public class LoginController extends HttpServlet {
 
         if (result > 0) {
             // Login success, redirect to the welcome page
-            response.sendRedirect("welcome.jsp");
+            HttpSession session = request.getSession();
+            DAOUser daouser=new DAOUser();
+            User user_logged= daouser.getUserByIdOrEmail(email);
+            session.setAttribute("user_logged", user_logged);
+            response.sendRedirect("JSP/welcome.jsp");
         } else {
             // Login failed, redirect back to the login page
             response.sendRedirect("JSP/login.jsp?error=1");
