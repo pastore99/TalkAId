e55e96ea5164dbccda67ddb2803266385b93d839
Sewaaa <sparno.samuele71@gmail.com>
2023-12-27 16:44:59 +0100

    Service ConditionData + Interface

---
+++ src/main/java/model/service/condition/ConditionData.java
@@ -0,0 +1,10 @@
+package model.service.condition;
+
+import model.DAO.DAOCondition;
+import model.entity.Condition;
+
+public class ConditionData implements ConditionDataInterface{
+
+    DAOCondition daoCondition=new DAOCondition();
+    public Condition getConditionByID(int id){return daoCondition.getConditionByID(id);}
+}
---
+++ src/main/java/model/service/condition/ConditionDataInterface.java
@@ -0,0 +1,9 @@
+package model.service.condition;
+
+import model.entity.Condition;
+import model.entity.User;
+
+public interface ConditionDataInterface {
+
+    public Condition getConditionByID(int id);
+}
