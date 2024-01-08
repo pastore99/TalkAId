1f1e81d240396ed4c68215cb3713b956bf720a22
Cristian Porzio <38082151+panuozzo77@users.noreply.github.com>
2024-01-08 21:26:21 +0100

    Revert "Merging 43 44 46"

--- src/main/java/controller/ExerciseController.java
+++ src/main/java/controller/ExerciseController.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Date;
 
 
 @WebServlet("/exerciseController")
@@ -16,9 +15,9 @@ public class ExerciseController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         ExerciseManager em = new ExerciseManager();
         String id = request.getParameter("exerciseID");
-        String insertionDate = request.getParameter("insertionDate");
-        request.getSession().setAttribute("insertionDate", Date.valueOf(insertionDate));
-        request.getSession().setAttribute("exerciseID", Integer.parseInt(id));
+        String insertiondate = request.getParameter("insertionDate");
+        request.getSession().setAttribute("insertionDate", insertiondate);
+        request.getSession().setAttribute("exerciseId", id);
 
         ExerciseGlossary ex = em.getExercise(Integer.parseInt(id));
         request.getSession().setAttribute("exercise", ex);
