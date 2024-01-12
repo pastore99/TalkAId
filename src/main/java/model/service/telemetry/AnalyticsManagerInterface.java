ee2b7827e595ef2102c8f2a782ce466ef28a7af7
panuozzo77 <totonnoyou59@gmail.com>
2024-01-12 22:17:53 +0100

    aggiunta interfaccia telemetria

---
+++ src/main/java/model/service/telemetry/AnalyticsManagerInterface.java
@@ -0,0 +1,16 @@
+package model.service.telemetry;
+
+/**
+ * Contiene metodi per il salvataggio e manipolazione di informazioni di telemetria degli utenti.
+ */
+public interface AnalyticsManagerInterface {
+
+    /**
+     * Salva i dati di telemetria di un utente, se possibile.
+     *
+     * @param userId      l'ID dell'utente.
+     * @param type        la tipologia di informazione salvata.
+     * @param description descrizione estesa dell'osservazione.
+     */
+    public void storeAnalytics(int userId, String type, String description);
+}
